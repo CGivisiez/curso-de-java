0e347e300f0be68793b7b1b9837d016ce88c718d
fcnmachado <fcnmachado@gmail.com>
2012-11-05 21:15:50 -0200

    Exercício de coleções 02

---
+++ estagiarios/felipe-machado/colecoes/colecoes02/Lembrete.java
@@ -0,0 +1,23 @@
+package colecoes02;
+
+import java.util.ArrayList;
+import java.util.List;
+
+//2 - Crie uma classe Lembrete com um atributo List<String> tarefas. Em um método main, crie um mapa cujas chaves serão os dias da semana (String) e 
+//os valores serão objetos da classe Lembrete. Escreva algum código demonstrando a estrutura criada.
+public class Lembrete {
+	List<String> tarefas;
+	
+	public Lembrete (){
+		tarefas = new ArrayList<String>();
+	}
+	public void adicionaTarefa(String tarefa){
+		tarefas.add(tarefa);
+	}
+	
+	public void imprimeListaTarefas(){
+		for(int i = 0; i < tarefas.size(); i++){
+			System.out.println(tarefas.get(i));
+		}
+	}	
+}
